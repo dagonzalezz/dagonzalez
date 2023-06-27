8086d6cd04901f6863045c8e50a9b96bfcf14e91
dagonzalezz <137910559+dagonzalezz@users.noreply.github.com>
2023-06-27 10:48:24 -0400

    Add files via upload

---
+++ #Funciones.py
@@ -0,0 +1,9 @@
+#Funciones
+
+lista_rut = []
+lista_nombre = []
+lista_identificador = []
+lista_nombre_mascota = []
+grabar = []
+validar_dias = []
+
---
+++ Gonzalez_Damian_pgy1121_p3_008d.py
@@ -0,0 +1,55 @@
+import numpy
+import time
+import msvcrt
+
+lista_rut = []
+lista_nombre = []
+lista_identificador = []
+lista_nombre_mascota = []
+grabar = []
+validar_dias = []
+
+
+print("Bienvenido a guarderia mascota segura")
+while True:
+        print("""
+        1.grabar
+        2.buscar
+        3.retirarse
+        4.salir""")
+
+
+if option1:
+        grabar = fn, entre ellos: 
+        Rutdueño =. (sin puntos ni dígito verificador, debe tener entre 7 y 8 dígitos)
+     Nombredueño =  (al menos debe tener 3 letras).
+ Identificadorúnico = (comienza en 1 y se crea de forma automática).
+    True:
+         if Nombremascota = 
+
+
+if option2:
+     if 
+    
+
+
+if option3:
+     if
+
+
+
+
+
+
+
+
+if option4:
+     
+
+rutdueño = input ("ingrese su rut sin puntos ni digito verificador:")
+
+nomdueño = input ("Ingrese su nombre, debe tener al menos 3 letras")
+        
+    
+    
+    
